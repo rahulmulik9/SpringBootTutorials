42e38c63d387cecb7b4a9d83a2db95e1c9639a5e
Rahul Mulik <rahulmulik9@gmail.com>
2023-07-30 17:47:01 +0530

    Initial Commit

---
+++ src/main/java/com/SpringBootTuto/Rahul_spring_demo/RahulSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootTuto.Rahul_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootTuto/Rahul_spring_demo/RahulSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootTuto.Rahul_spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahulSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
