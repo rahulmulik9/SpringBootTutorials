4c04f5e4bddab36c6d3af624190460f55908f679
Rahul Mulik <rahulmulik9@gmail.com>
2023-07-31 21:52:57 +0530

    Commented in controller

--- src/main/java/com/SpringBootTuto/Rahul_spring_demo/StudentController.java
+++ src/main/java/com/SpringBootTuto/Rahul_spring_demo/StudentController.java
@@ -10,7 +10,12 @@ import java.util.List;
 @RestController
 @RequestMapping( "api")
 public class StudentController {
-
+    //simple crud operation
+    //include simple class student with Id as primary class and student name and student subject
+    //include get all(Get Mapping)
+    //save student (Post Mapping)
+    //delete student (Delete Mapping)
+    //update student (Put Mapping)
     @Autowired
     private final StudentService studentService;
 
@@ -18,19 +23,23 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-
+      //getting all the student
     @GetMapping("/all")
     public List<StudentEntity> getAllStudent(){
      List<StudentEntity> studentList = studentService.getAllStudent();
      return studentList ;
     }
 
+
+    //saving student
     @PostMapping("/save")
     public StudentEntity createStudent(@RequestBody StudentEntity studentEntity){
         StudentEntity student= studentService.createStudent(studentEntity);
         return student;
     }
 
+
+    //Delet student
     @DeleteMapping("/delet/{id}")
     public String deletStudent(@PathVariable Long id){
         String resp =studentService.deletStudent(id);
@@ -38,6 +47,8 @@ public class StudentController {
 
     }
 
+
+    //update student
     @PutMapping("/update/{id}")
     public StudentEntity updateStudent(@PathVariable long id ,@RequestBody StudentEntity student){
         StudentEntity studentnew= studentService.updateStudent(id,student);
