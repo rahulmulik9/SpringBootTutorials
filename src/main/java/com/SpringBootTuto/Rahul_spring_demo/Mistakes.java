a136235654f774237643b7240f5b7dc4e1dc4901
Rahul Mulik <rahulmulik9@gmail.com>
2023-07-31 21:47:39 +0530

    Mistake class

---
+++ src/main/java/com/SpringBootTuto/Rahul_spring_demo/Mistakes.java
@@ -0,0 +1,7 @@
+package com.SpringBootTuto.Rahul_spring_demo;
+
+public class Mistakes {
+    // 1] cross verify data type of id in entity class..i was put as int and in delet method i was searching as findById and passing as long ID
+    // 2] Id you dont put @Data annotaino over entity class..while debugging data will be fetched but in postman it will come as empty array
+
+}
